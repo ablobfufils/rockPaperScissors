2a5f5b4a7dfcc93b78b4dfb947f238451758a33a
ablobfufils <coltermd@gmail.com>
2020-05-05 20:28:13 -0300

    starting script.js and changed div id=humanChoices to <buttons> instead of <input type=image>

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Declare the 3 choices of RPS
 const choices = ['rock', 'paper', 'scissors'];
 
-
+function playGame(){
+    let playerChoice = document.getElementsByClassName("humanChoices").value
+}
 
 
 
